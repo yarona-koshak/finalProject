d23b43fdcc29a8c3c6d80c5c01789bb2b82a2e62
yarona-koshak <s332877679@ariel.ort.org.il>
2025-03-15 22:24:23 +0200

    ru index and sign up

--- website/pages/index.ts
+++ website/pages/index.ts
@@ -6,3 +6,16 @@ toLoginPage.onclick = function () {
 toSignUpPage.onclick = function () {
     window.location.href = "/website/pages/signUp.html";
 };
+// עברת עמודים של login and sign up 
+let leanguageChooise= document.getElementById("leanguageChooise") as HTMLSelectElement;
+let opteng= document.getElementById("opteng") as HTMLOptionElement;
+let optru= document.getElementById("optru") as HTMLOptionElement;
+
+leanguageChooise.onchange=function(){
+    if (leanguageChooise.value.toString() === optru.value.toString()) {
+        window.location.href = "/website/pages/indexru.html";
+    }
+    else{
+        window.location.href = "/website/pages/index.html";
+    }
+}
