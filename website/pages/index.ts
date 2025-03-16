10a275896b532b3629b399e1df770ba69da2f29d
yarona-koshak <s332877679@ariel.ort.org.il>
2025-03-16 17:10:27 +0200

    ddd

--- website/pages/index.ts
+++ website/pages/index.ts
@@ -1,3 +1,18 @@
+
+// בחירת שפה ועברה לעמוד מתאים לפי השפה 
+let leanguageChooise= document.getElementById("leanguageChooise") as HTMLSelectElement;
+let opteng= document.getElementById("opteng") as HTMLOptionElement;
+let optru= document.getElementById("optru") as HTMLOptionElement;
+
+leanguageChooise.onchange=function(){
+    if (leanguageChooise.value.toString() === optru.value.toString()) {
+        window.location.href = "/website/pages/indexru.html";
+    }
+    else{
+        window.location.href = "/website/pages/index.html";
+    }
+}
+
 // עברה לעמודים אחרים באמצעות כפתורים
 
 // login , sign up:
@@ -14,27 +29,20 @@ toSignUpPage.onclick = function () {
 let tobillboardPage = document.querySelector("#billboard") as HTMLButtonElement;
 let toOrderTicketsPage= document.querySelector("#orderTickets") as HTMLButtonElement;
 let toarchivesPage= document.querySelector("#archives") as HTMLButtonElement;
+
 toarchivesPage.onclick = function () {
     window.location.href = "/website/pages/archives.html";
 };
+
 toOrderTicketsPage.onclick = function () {
-    window.location.href = "/website/pages/order_tickets.html";
+      if (leanguageChooise.value.toString() === optru.value.toString()) {
+        window.location.href = "/website/pages/orderTicketsru.html";
+    }
+    else{
+        window.location.href = "/website/pages/order_tickets.html";
+    } 
 };
+
 tobillboardPage.onclick = function () {
     window.location.href = "/website/pages/billboard.html";
 };
-
-
-// בחירת שפה ועברה לעמוד מתאים לפי השפה 
-let leanguageChooise= document.getElementById("leanguageChooise") as HTMLSelectElement;
-let opteng= document.getElementById("opteng") as HTMLOptionElement;
-let optru= document.getElementById("optru") as HTMLOptionElement;
-
-leanguageChooise.onchange=function(){
-    if (leanguageChooise.value.toString() === optru.value.toString()) {
-        window.location.href = "/website/pages/indexru.html";
-    }
-    else{
-        window.location.href = "/website/pages/index.html";
-    }
-}
