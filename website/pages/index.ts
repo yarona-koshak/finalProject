3c11a4f26b06ba44352d92495c45e3505883c8f1
yarona-koshak <s332877679@ariel.ort.org.il>
2025-03-16 12:31:04 +0200

    a , b ,o html

--- website/pages/index.ts
+++ website/pages/index.ts
@@ -1,3 +1,6 @@
+// עברה לעמודים אחרים באמצעות כפתורים
+
+// login , sign up:
 let toSignUpPage = document.querySelector("#signUp") as HTMLButtonElement;
 let toLoginPage= document.querySelector("#login") as HTMLButtonElement;
 toLoginPage.onclick = function () {
@@ -6,7 +9,23 @@ toLoginPage.onclick = function () {
 toSignUpPage.onclick = function () {
     window.location.href = "/website/pages/signUp.html";
 };
-// עברת עמודים של login and sign up 
+
+// archives , billboard , order_tickets:
+let tobillboardPage = document.querySelector("#billboard") as HTMLButtonElement;
+let toOrderTicketsPage= document.querySelector("#orderTickets") as HTMLButtonElement;
+let toarchivesPage= document.querySelector("#archives") as HTMLButtonElement;
+toarchivesPage.onclick = function () {
+    window.location.href = "/website/pages/archives.html";
+};
+toOrderTicketsPage.onclick = function () {
+    window.location.href = "/website/pages/order_tickets.html";
+};
+tobillboardPage.onclick = function () {
+    window.location.href = "/website/pages/billboard.html";
+};
+
+
+// בחירת שפה ועברה לעמוד מתאים לפי השפה 
 let leanguageChooise= document.getElementById("leanguageChooise") as HTMLSelectElement;
 let opteng= document.getElementById("opteng") as HTMLOptionElement;
 let optru= document.getElementById("optru") as HTMLOptionElement;
