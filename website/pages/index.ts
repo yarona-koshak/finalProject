36e709a6a65eda4970c370869b57013b4c1d2576
yarona shneider <s332877679@ariel.ort.org.il>
2025-04-23 09:04:01 +0300

    גגגג

--- website/pages/index.ts
+++ website/pages/index.ts
@@ -1,3 +1,4 @@
+import { send } from "../utilities";
 
 // בחירת שפה ועברה לעמוד מתאים לפי השפה 
 let leanguageChooise= document.getElementById("leanguageChooise") as HTMLSelectElement;
@@ -48,3 +49,26 @@ tobillboardPage.onclick = function () {
 toarchivesPage.onclick = function () {
     window.location.href = "/website/pages/archives.html";
 };
+// 
+let loggedOutDiv = document.getElementById("loggedOutDiv") as HTMLDivElement;
+let logInButton = document.getElementById("logInButton") as HTMLButtonElement;
+let signUpButton = document.getElementById("signUpButton") as HTMLButtonElement;
+let loggedInDiv = document.getElementById("loggedInDiv") as HTMLDivElement;
+let greetingDiv = document.getElementById("greetingDiv") as HTMLDivElement;
+let logOutButton = document.getElementById("logOutButton") as HTMLButtonElement;
+let userId = localStorage.getItem("userId");
+
+let username = await send("getUsername", userId) as string | null;
+
+if (username != null) {
+  greetingDiv.innerText = "Welcome, " + username + "!";
+  loggedInDiv.classList.remove("hidden");
+} else {
+  let userIdExists = localStorage.getItem("userId") != null;
+  localStorage.removeItem("userId");
+  loggedOutDiv.classList.remove("hidden");
+
+  if (userIdExists) {
+    top!.location.reload();
+  }
+}
