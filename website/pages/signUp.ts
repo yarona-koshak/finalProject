517c399bfe4ada668ff2f9def8d994f47ab0f10e
yarona-koshak <s332877679@ariel.ort.org.il>
2025-06-03 21:39:48 +0300

    sign up problrms

--- website/pages/signUp.ts
+++ website/pages/signUp.ts
@@ -4,6 +4,7 @@ let usernameInput = document.getElementById("usernameInput") as HTMLInputElement
 let passwordInput = document.getElementById("passwordInput") as HTMLInputElement;
 let submitButton = document.getElementById("submitButton") as HTMLButtonElement;
 let messageDiv = document.getElementById("messageDiv") as HTMLDivElement;
+let confettiContainer = document.getElementById("confetti-container") as HTMLDivElement;
 
 submitButton.onclick = async function () {
   let userId = await send("signUp", [
@@ -13,8 +14,42 @@ submitButton.onclick = async function () {
 
   if (userId != null) {
     localStorage.setItem("userId", userId);
-    location.href = "index.html";
+    triggerConfettiEffect();
   } else {
     messageDiv.innerText = "Username is already taken";
   }
 };
+
+function triggerConfettiEffect() {
+  let i = 0;
+  while (i < 40) {
+    let confetti = document.createElement("div");
+    confetti.classList.add("confetti");
+
+    let colors = ["red", "blue", "green", "yellow", "purple", "orange"];
+    let colorIndex = Math.floor(Math.random() * colors.length);
+    confetti.style.backgroundColor = colors[colorIndex];
+
+    let left = Math.random() * window.innerWidth;
+    confetti.style.left = left + "px";
+
+    let size = 6 + Math.random() * 12;
+    confetti.style.width = size + "px";
+    confetti.style.height = size + "px";
+
+    confettiContainer.appendChild(confetti);
+
+    setTimeout(function () {
+      if (confettiContainer.contains(confetti)) {
+        confettiContainer.removeChild(confetti);
+      }
+    }, 2000);
+
+    i = i + 1;
+  }
+
+
+  setTimeout(function () {
+    location.href = "index.html";
+  }, 4000);
+}
