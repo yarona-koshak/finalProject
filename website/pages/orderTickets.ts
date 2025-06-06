8a3d115608438f092cb3b89a9e44238eb87b416e
yarona-koshak <s332877679@ariel.ort.org.il>
2025-06-06 22:29:07 +0300

    last touch

--- website/pages/orderTickets.ts
+++ website/pages/orderTickets.ts
@@ -28,13 +28,15 @@ select.addEventListener("change", () => {
     artistinfodiv.style.display = "none";
   }
 });
+
+let confettiContainer = document.getElementById("confetti-container") as HTMLDivElement;
 let nameInput= document.getElementById("name") as HTMLInputElement;
 orderBtn.onclick = function() {
   if (nameInput.value != ""){
   if (confirm("Do you want to order")) {
       order();
   box.classList.add('box2');
-
+    triggerConfettiEffect();
   box.addEventListener('transitionend', () => {
     setTimeout(() => {
       alert("Congratulations!");
@@ -72,6 +74,34 @@ async function order() {
 
 
 
+function triggerConfettiEffect() {
+  let i = 0;
+  while (i < 40) {
+    let confetti = document.createElement("div");
+    confetti.classList.add("confetti");
+
+    let colors = ["red", "blue", "green", "yellow", "purple", "orange"];
+    let colorIndex = Math.floor(Math.random() * colors.length);
+    confetti.style.backgroundColor = colors[colorIndex];
+
+    let left = Math.random() * window.innerWidth;
+    confetti.style.left = left + "px";
+
+    let size = 6 + Math.random() * 12;
+    confetti.style.width = size + "px";
+    confetti.style.height = size + "px";
+
+    confettiContainer.appendChild(confetti);
+
+    setTimeout(function () {
+      if (confettiContainer.contains(confetti)) {
+        confettiContainer.removeChild(confetti);
+      }
+    }, 2000);
+
+    i++;
+  }
+}
 
 
 
